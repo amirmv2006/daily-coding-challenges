593ec52038b63f91d6d445095096002ca4e39d87
Amir <AmirMV2006@gmail.com>
2019-07-07 22:11:24 +0200

    added the question

--- july-challenges/src/main/java/ir/amv/os/dcc/july/seventh/Node.java
+++ july-challenges/src/main/java/ir/amv/os/dcc/july/seventh/Node.java
@@ -5,11 +5,28 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 import java.util.function.Predicate;
 
+/**
+ * Good morning! Here's your coding interview problem for today.
+ *
+ * This problem was asked by Google.
+ *
+ * Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, and deserialize(s), which deserializes the string back into the tree.
+ *
+ * For example, given the following Node class
+ *
+ * class Node:
+ *     def __init__(self, val, left=None, right=None):
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ * The following test should pass:
+ *
+ * node = Node('root', Node('left', Node('left.left')), Node('right'))
+ * assert deserialize(serialize(node)).left.left.val == 'left.left'
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor

3bcddc289ae50209793307a89b4abe69dadec269
Amir Vosough <amir.vosough@tba.group>
2019-07-16 15:24:33 +0200

    added the challenge to the docs

---
+++ july-challenges/src/main/java/ir/amv/os/dcc/july/fourteenth/JobScheduler.java
@@ -0,0 +1,13 @@
+package ir.amv.os.dcc.july.fourteenth;
+
+/**
+ * Good morning! Here's your coding interview problem for today.
+ *
+ * This problem was asked by Apple.
+ *
+ * Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
+ * @author Amir
+ */
+public class JobScheduler {
+
+}

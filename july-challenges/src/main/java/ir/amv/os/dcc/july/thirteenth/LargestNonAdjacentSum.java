c0dc5876f01ab90eda559a0fbd779cfd081a3734
Amir <AmirMV2006@gmail.com>
2019-08-11 22:50:58 +0200

    Draft create

---
+++ july-challenges/src/main/java/ir/amv/os/dcc/july/thirteenth/LargestNonAdjacentSum.java
@@ -0,0 +1,27 @@
+package ir.amv.os.dcc.july.thirteenth;
+
+/**
+ * Good morning! Here's your coding interview problem for today.
+ *
+ * This problem was asked by Airbnb.
+ *
+ * Given a list of integers, write a function that returns the largest sum of non-adjacent numbers. Numbers can be 0 or negative.
+ *
+ * For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1, 1, 5] should return 10, since we pick 5 and 5.
+ *
+ * Follow-up: Can you do this in O(N) time and constant space?
+ */
+public class LargestNonAdjacentSum {
+
+    private int findLargestNonAdjacentSum(int[] numbers) {
+        int happy = 0;
+        int unhappy = 0;
+        for (int i = 0; i < numbers.length; i++) {
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
